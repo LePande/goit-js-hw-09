15f6ca2785ece9c630d3fe810238d01c9725cab9
LePande <119473273+LePande@users.noreply.github.com>
2023-10-13 00:05:13 -0500

    Organizo codigo tarea 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,40 +13,38 @@ const options = {
       alert('Por favor, elige una fecha en el futuro.');
     } else {
       document.querySelector('[data-start]').removeAttribute('disabled');
+
+      const endDate = selectedDate;
+      const timerInterval = 1000;
+
+      function updateTimer() {
+        const currentTime = new Date();
+        const timeLeft = endDate - currentTime;
+
+        if (timeLeft <= 0) {
+          clearInterval(timerId);
+          alert('¡Tiempo agotado!');
+          return;
+        }
+
+        const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+        document.querySelector('[data-days]').textContent =
+          addLeadingZero(days);
+        document.querySelector('[data-hours]').textContent =
+          addLeadingZero(hours);
+        document.querySelector('[data-minutes]').textContent =
+          addLeadingZero(minutes);
+        document.querySelector('[data-seconds]').textContent =
+          addLeadingZero(seconds);
+      }
+
+      const timerId = setInterval(updateTimer, timerInterval);
     }
   },
 };
 
 flatpickr('#datetime-picker', options);
-
-document.querySelector('[data-start]').addEventListener('click', () => {
-  const endDate = flatpickr.parseDate(
-    document.querySelector('#datetime-picker').value
-  );
-  const timerInterval = 1000;
-
-  function updateTimer() {
-    const currentTime = new Date();
-    const timeLeft = endDate - currentTime;
-
-    if (timeLeft <= 0) {
-      clearInterval(timerId);
-      alert('¡Tiempo agotado!');
-      return;
-    }
-
-    const { days, hours, minutes, seconds } = convertMs(timeLeft);
-
-    document.querySelector('[data-days]').textContent = addLeadingZero(days);
-    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
-    document.querySelector('[data-minutes]').textContent =
-      addLeadingZero(minutes);
-    document.querySelector('[data-seconds]').textContent =
-      addLeadingZero(seconds);
-  }
-
-  const timerId = setInterval(updateTimer, timerInterval);
-});
 
 function convertMs(ms) {
   const second = 1000;
@@ -56,8 +54,8 @@ function convertMs(ms) {
 
   const days = Math.floor(ms / day);
   const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const minutes = Math.floor((ms % hour) / minute);
+  const seconds = Math.floor((ms % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
